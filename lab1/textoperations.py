3e229a0d5ff4612ba7324a2cf913d928b5a5a68f
John Shaw <j.j.g.shaw@gmail.com>
2016-01-28 21:41:06 +0100

    Added testfiles with the example from the slides and correct filename suffix. Wrote comments for textoperations.py

--- lab1/textoperations.py
+++ lab1/textoperations.py
@@ -1,26 +1,27 @@
-Language = "abcdefghijklmnopqrstuvwxyzåäö ,."
+Alphabet = "abcdefghijklmnopqrstuvwxyzåäö ,."
 
+#This function formats the text by removing chacters not in the alphabet
 def validateString(inputString):
     inputString = list(inputString)
     returnString = ""
     for char in inputString:
-        if char not in Language:
+        if char not in Alphabet:
             print("Character not recognized: " + char + " Removing the char")
         else:
             returnString = returnString + char
     return returnString
 
+#Lowercase and removes non alphabet characters
 def formatString(inputString):
     inputString.lower()
     return validateString(inputString)
 
+#Takes a filenName as a string and returns the contents as a string.
 def getFileAsString(fileName):
     with open(fileName, 'r') as fileObject:
         data = fileObject.read()
     return data
     
-def test(fileName):
-    print(getFileAsStringList(fileName))
-    
-def test2(fileName):
-    print(formatString(getFileAsStringList(fileName)))
+#The input for the encryption algorithm.	
+def getStringForEncryption(fileName):
+	return formatString(getFileAsString)
