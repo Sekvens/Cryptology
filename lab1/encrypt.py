498885f6cab82abea694ce46d3568b02cb48e7d3
John Shaw <j.j.g.shaw@gmail.com>
2016-01-29 09:42:44 +0100

    Untested encryption function added.

--- lab1/encrypt.py
+++ lab1/encrypt.py
@@ -15,9 +15,18 @@ encryptedText = ""
 print("Filename given is: " + nonSafeFileName)
 
 def encrypt(plaintext):
+	plaintextIndex = 0
+	keyLength = len(key)
 	for char in plaintext:
+		#calculate what key character to use and look up the number in the alphabet
+		keyInteger = Alphabet.index(key[plaintextIndex % keyLength])
+		#Fetching the current char's integer representative
+		plainTextInteger = Alphabet.index[char]
+		plaintextIndex += 1
+		encryptedChar = Alphabet[(plainTextInteger + keyInteger) % Alphabet]
+		encryptedText += encryptedChar
+	return encryptedText
 		
-
 if(plaintext != ""):
 	plaintext = textoperations.getStringForEncryption(fileName)
 	print encrypt(plaintext)
