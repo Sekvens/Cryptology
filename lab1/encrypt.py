6826f0549926c98e476f5f679975594ecc828a20
Teloch <j.j.g.shaw@gmail.com>
2016-01-30 14:57:54 +0100

    Heavy refactorisation and added pythondoc

--- lab1/encrypt.py
+++ lab1/encrypt.py
@@ -2,72 +2,80 @@ import textoperations
 import sys
 import os
 import codecs
-
-nonSafeFileName = ""
-plainText = ""
-key = ""
-encryptedText = ""
-nonSafeOutput = ""
-
-if(len(sys.argv) >= 2):
-    nonSafeFileName = sys.argv[1]
-    plainText = textoperations.getStringForEncryption(nonSafeFileName)
-    
-if(len(sys.argv) >= 3):
-    nonSafeKeyFile = sys.argv[2]
-    key = textoperations.getStringForEncryption(nonSafeKeyFile)
-    
-if(len(sys.argv) == 4):
-    nonSafeOutput = sys.argv[3]
     
 #In python a string is a list. By calling Alphabet[x] we get the character for that number sequence. This we can use as our character -> number mapping.
 Alphabet = textoperations.getAlphabet()
 
-print("Filename given is: " + nonSafeFileName)
-
-def encrypt2(plaintext):
-    plaintextIndex = 0
-    keyLength = len(key)
-    encryptedText = ""
-    if(keyLength == 0):
-        return "Key missing"
-    for char in plaintext:
-        #calculate what key character to use and look up the number in the alphabet
-        keyInteger = Alphabet.index(key[plaintextIndex % keyLength])
-        #Fetching the current char's integer 
-        plainTextInteger = Alphabet.find(char)
-        plaintextIndex += 1
-        print(plainTextInteger)
-        print(keyInteger)
-        encryptedChar = Alphabet[(plainTextInteger + keyInteger) % len(Alphabet)]
-        encryptedText = encryptedText + encryptedChar
-    return encryptedText
-
-def decrypt(plaintext):
-    plaintextIndex = 0
-    keyLength = len(key)
-    encryptedText = ""
-    if(keyLength == 0):
-        return "Key missing"
-    for char in plaintext:
-        #calculate what key character to use and look up the number in the alphabet
-        keyInteger = Alphabet.index(key[plaintextIndex % keyLength])
-        #Fetching the current char's integer 
-        plainTextInteger = Alphabet.find(char)
-        plaintextIndex += 1
-        print(plainTextInteger)
-        print(keyInteger)
-        encryptedChar = Alphabet[(plainTextInteger - keyInteger) % len(Alphabet)]
-        encryptedText = encryptedText + encryptedChar
-    return encryptedText    
+def vigenereCipher(inputString, keyString, encrypt):
+    """
+    Cipher a string with the Vigenere cipher.
     
-if(plainText != ""):
-    print("Found plaintext, performing encryption.")
-    print("plaintext is:" )
-    if(os.path.exists(nonSafeOutput)):
-        with open(nonSafeOutput, 'w', encoding='utf-8') as outputFile:
-            encryptionString = textoperations.getStringForEncryption(nonSafeFileName)
-            outputFile.write(encrypt2(encryptionString))
-        print("File written successfully.")
+    @type inputString: String
+    @type inputString: The text to encrypt or decrypt.
+    @type keyString: String
+    @type keyString: The key for the cipher.
+    @type encrypt: Boolean
+    @param encrypt: If the algorithm should encrypt or decrypt.
+    @rtype: String
+    """
+    inputIndex = 0
+    keyLength = len(keyString)
+    outText = ""
+    if(keyLength == 0):
+        print("Key missing")
+        return "Key missing"
+    for char in inputString:
+        #calculate what key character to use and look up the number in the alphabet
+        keyInteger = Alphabet.index(keyString[inputIndex % keyLength])
+        #Fetching the current char's integer 
+        inputInteger = Alphabet.find(char)
+        inputIndex += 1
+        if(encrypt):
+            outChar = Alphabet[(inputInteger + keyInteger) % len(Alphabet)]
+        else:
+            outChar = Alphabet[(inputInteger - keyInteger) % len(Alphabet)]
+        outText = outText + outChar
+    return outText
+
+def encrypt(plaintext, key):
+    return vigenereCipher(plaintext, key, True)
+
+def decrypt(encryptedText, key):
+    return vigenereCipher(encryptedText, key, False)
+    
+def doCrypt(inFile, keyFile, outFile, encryptB):
+    if(os.path.exists(inFile) and os.path.exists(keyFile)):
+        inString = textoperations.getFormattedStringFromFile(inFile)
+        keyString = textoperations.getFormattedStringFromFile(keyFile)
+        outString = ""
+        if(encryptB):
+            outString = encrypt(inString, keyString)
+        else:
+            outString = decrypt(inString, keyString)
+        textoperations.printOutputToFile(outFile, outString)
     else:
-        print("Output not found for: " + nonSafeOutput)
+        print("Didn't find the keyfile or the inFile file.")
+    
+def doEncrypt(plaintextFile, keyFile, outFile):
+    """
+    Takes text from a file and writes a encrypted version of it in another file.
+    @type plaintextFile: String
+    @param plaintextFile: A string with a filename containing the plaintext.
+    @type keyFile: String
+    @param keyFile: A string with a filename containing the key.
+    @type outFile: String
+    @param outFile: A string with a filename where the result will be written.
+    """
+    doCrypt(plaintextFile, keyFile, outFile, True)
+        
+def doDecrypt(encryptedTextFile, keyFile, outFile):
+    """
+    Takes an encryped text from a file and writes a decrypted version in another file.
+    @type encryptedTextFile: String
+    @param encryptedTextFile: A string with a filename containing the encrypted text.
+    @type keyFile: String
+    @param keyFile: A string with a filename containing the key.
+    @type outFile: String
+    @param outFile: A string with a filename where the result will be written.
+    """
+    doCrypt(encryptedTextFile, keyFile, outFile, False)
